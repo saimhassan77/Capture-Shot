e49633a42b5ca95156cbc2cc78ab5bb12400cb9d
saimhassan77 <saimhassan7855@gmail.com>
2026-04-10 11:37:54 +0500

    Add rate-limit middleware to limit screenshot requests

---
+++ ScreenshotAPI/src/middleware/screenshotLimit.middleware.js
@@ -0,0 +1,18 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import { rateLimit } from 'express-rate-limit'
+
+const limit = asyncHandler(
+    rateLimit({
+        windowMs: 60 * 1000, // 1 minute
+        max: 4, // max 5 requests per IP
+        standardHeaders: true,
+        legacyHeaders: false,
+        message: {
+            success: false,
+            message: "Too many screenshot requests, try again in 1 minute"
+        }
+    })
+)
+
+export default limit
--- ScreenshotAPI/src/routers/screenshotAPI.router.js
+++ ScreenshotAPI/src/routers/screenshotAPI.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { takeScreenshot } from "../controllers/screenshotAPI.controller.js";
+import limit from "../middleware/screenshotLimit.middleware.js";
 
 const router=Router();
 
-router.route("/screenshot").post(takeScreenshot)
+router.route("/screenshot").post(limit, takeScreenshot)
 
 export default router
